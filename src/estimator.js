cfd03c3e42a039bd002f266d0c03b764af99b985
wathigo <wathigosimon@gmail.com>
2020-04-07 09:38:08 -0400

    Fixes linter errors

--- src/estimator.js
+++ src/estimator.js
@@ -1,11 +1,11 @@
-const covid19ImpactEstimator = (data => {
+const covid19ImpactEstimator = ((data) => {
   const impact = {};
   const severeImpact = {};
   impact.currentlyInfected = data.reportedCases * 10;
   severeImpact.currentlyInfected = data.reportedCases * 50;
-  impact.infectionsByRequestedTime = impact.currentlyInfected * Math.pow(2, 10)
-  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * Math.pow(2, 10)
-  return {data, impact, severeImpact}
+  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** 10);
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** 10);
+  return { data, impact, severeImpact };
 });
 
 export default covid19ImpactEstimator;
