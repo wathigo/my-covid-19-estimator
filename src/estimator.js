9a9c94b267bbc583a42d532ea57fdd645e1258c6
wathigo <wathigosimon@gmail.com>
2020-04-07 09:30:40 -0400

    Implement covid19ImpactEstimator function body

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,11 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data => {
+  impact = {};
+  severeImpact = {};
+  impact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * Math.pow(2, 10)
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * Math.pow(2, 10)
+  return {data, impact, severeImpact}
+});
 
 export default covid19ImpactEstimator;
