9f67e677546987c679d565122fc4de2aeddfa61b
wathigo <wathigosimon@gmail.com>
2020-04-07 10:30:30 -0400

    Updates logic to calculate infectionsByRequestedTime

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,26 @@
+const infectionsByRequestedTime = (data, currentlyInfected) => {
+  let infections = null;
+  switch (data.periodType) {
+    case 'weeks':
+      infections = currentlyInfected * (2 ** 70);
+      break;
+    case 'months':
+      infections = currentlyInfected * (2 ** 300);
+      break;
+    default:
+      infections = currentlyInfected * (2 ** 10);
+      break;
+  }
+  return infections;
+};
+
 const covid19ImpactEstimator = ((data) => {
   const impact = {};
   const severeImpact = {};
   impact.currentlyInfected = data.reportedCases * 10;
   severeImpact.currentlyInfected = data.reportedCases * 50;
-  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** 10);
-  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** 10);
+  impact.infectionsByRequestedTime = infectionsByRequestedTime(data, impact.currentlyInfected);
+  severeImpact.infectionsByRequestedTime = infectionsByRequestedTime(data, severeImpact.currentlyInfected);
   return { data, impact, severeImpact };
 });
 
