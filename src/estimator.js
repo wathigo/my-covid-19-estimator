1ddacb10ce6ff8960a0ee2aa1c324b1cb0af7c51
wathigo <wathigosimon@gmail.com>
2020-04-07 13:24:53 -0400

    Adds casesForICUByRequestedTime method

--- src/estimator.js
+++ src/estimator.js
@@ -27,6 +27,9 @@ const infectionsByRequestedTime = (data, currentlyInfected) => {
 const severeCasesByRequestedTime = ((time) => Math.round(time * 0.15)
 );
 
+const casesForICUByRequestedTime = ((time) => Math.round(time * 0.05)
+);
+
 const hospitalBedsByRequestedTime = (data, severeCases) => {
   const availableBeds = data.totalHospitalBeds * 0.35;
   return availableBeds - severeCases;
@@ -36,19 +39,27 @@ const covid19ImpactEstimator = ((data) => {
   const impact = {};
   const severeImpact = {};
   const reportedCases = Big(data.reportedCases);
+
   impact.currentlyInfected = reportedCases * Big(10);
   severeImpact.currentlyInfected = reportedCases * Big(50);
+
   const iCurrentlyInfected = Big(impact.currentlyInfected);
   impact.infectionsByRequestedTime = infectionsByRequestedTime(data, iCurrentlyInfected);
   const siCurrentlyInfected = Big(severeImpact.currentlyInfected);
   severeImpact.infectionsByRequestedTime = infectionsByRequestedTime(data, siCurrentlyInfected);
+
   impact.severeCasesByRequestedTime = severeCasesByRequestedTime(impact.infectionsByRequestedTime);
-  const time = severeImpact.infectionsByRequestedTime;
+  let time = severeImpact.infectionsByRequestedTime;
   severeImpact.severeCasesByRequestedTime = severeCasesByRequestedTime(time);
+
   const siSevereCases = severeImpact.severeCasesByRequestedTime;
   const iSevereCases = impact.severeCasesByRequestedTime;
   impact.hospitalBedsByRequestedTime = hospitalBedsByRequestedTime(data, iSevereCases);
   severeImpact.hospitalBedsByRequestedTime = hospitalBedsByRequestedTime(data, siSevereCases);
+
+  impact.casesForICUByRequestedTime = casesForICUByRequestedTime(impact.infectionsByRequestedTime);
+  time = severeImpact.infectionsByRequestedTime;
+  severeImpact.casesForICUByRequestedTime = casesForICUByRequestedTime(time);
   return { data, impact, severeImpact };
 });
 
