118075ef807e5336b6425e162663b5d7bd78d16b
Simon Wathigo <wathigosimon@gmail.com>
2020-04-14 11:38:38 +0300

    Updates calculations

--- src/estimator.js
+++ src/estimator.js
@@ -57,7 +57,7 @@ const dollarsInFlight = (data, infections) => {
 
 const hospitalBedsByRequestedTime = (data, severeCases) => {
   const availableBeds = data.totalHospitalBeds * 0.35;
-  return Math.floor(availableBeds - severeCases);
+  return availableBeds - severeCases;
 };
 
 const covid19ImpactEstimator = ((data) => {
